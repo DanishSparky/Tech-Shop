3d11a1ca1d43dc67cafed721f6bfe531e36bf248
DanishSparky <danishsparkydt@gmail.com>
2026-02-12 02:01:22 +0530

    Problem in rendering all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes } from 'react-router'
-import Routeing from './routing/Routeing'
+import NavBar from "./Components/Navbar"
+
+import Routings from './Routing/Routings'
+
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Routeing/>
+      <NavBar/>
+      <Routings />
     </>
   )
 }
